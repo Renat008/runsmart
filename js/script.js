b9bbfe4f56ec2f8816dc5e45ac20e990d9ef1ada
Renat <kuzminrenat51@mail.ru>
2024-07-05 17:14:04 +0300

    slider

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// $(document).ready(function(){
+//     $('.carousel__inner').slick({
+//         // dots: true,
+//         speed: 1000,
+//         // adaptiveHeight: true,
+//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+//         responsive: [{
+//             breakpoint: 992,
+//             settings: {
+//               dots: true,
+//               arrows: false
+//             }
+//         }]
+//         // autoplay: true,
+//         // autoplaySpeed: 2000,
+//         // fade: true,
+//         // cssEase: 'linear'
+//       });
+// });
+
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    controls: false,
+    autoplay: false,
+    nav: false,
+  });
+
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+});
